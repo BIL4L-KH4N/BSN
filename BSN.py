c87b4ef41ca269c51c1b3330b92c91c0957b84fd
BIL4L-KH4N <90181951+BIL4L-KH4N@users.noreply.github.com>
2021-10-24 10:34:17 +0500

    Update BSN.py

--- BSN.py
+++ BSN.py
@@ -3,4 +3,4 @@ os.system('clear')
 print'\x1b[1;37m\n\x1b[1;94m########  #### ##        #########  ##\n\x1b[1;94m##     ##  ##  ##       ##       ## ##\n\x1b[1;92m##     ##  ##  ##       ##       ## ##\n\x1b[1;92m########   ##  ##       ########### ##\n\x1b[1;92m##     ##  ##  ##       ##       ## ##\n\x1b[1;94m##     ##  ##  ##       ##       ## ##\n\x1b[1;94m########  #### ######## ##       ## ########\n\x1b[1;92m-----------------------------------------------\n\x1b[1;92m[\xe2\x9c\x93] \x1b[1;93mMAKER       :   BILAL KHAN\n\x1b[1;92m[\xe2\x9c\x93]\x1b[1;93m FACEBOOK    :   \xe2\x88\x86\xe2\x88\x86\xe2\x88\x86\xe2\x88\x86\xe2\x88\x86\xe2\x88\x86\xe2\x88\x86\xe2\x88\x86\n\x1b[1;92m[\xe2\x9c\x93] \x1b[1;93mWHATSAPP    :   +923485664243\n\x1b[1;92m[\xe2\x9c\x93] \x1b[1;96mChange Is The Unchangeable Law Of Nature\n\x1b[1;92m-----------------------------------------------'
 print''
 print''
-print'\x1b[1;32mWAIT FOR UPDATE-- updating.... 
+print'\x1b[1;32mWAIT FOR UPDATE-- updating.... '
