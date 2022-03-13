6d3d076c47e45de1fd9d3df03fec68cfad0c8206
BIL4L-KH4N <90181951+BIL4L-KH4N@users.noreply.github.com>
2022-03-13 15:25:00 +0500

    Delete bsn.py

--- bsn.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system('termux-setup-storage')
-os.system('git pull')
-os.system('rm -rf file32.cpython-310.so')
-os.system('rm -rf file.cpython-310.so')
-from file32 import reg
-reg()
